95721c361476b17d933aee7a7ed990ec9b8e646a
Fabricio Dias <fabricio270909@gmail.com>
2025-01-20 13:43:01 -0300

    Reading and print file content

--- DemoFileStream/Program.cs
+++ DemoFileStream/Program.cs
@@ -1,9 +1,33 @@
-﻿namespace DemoFileStream;
+﻿using System.IO;
+
+namespace DemoFileStream;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string path = @"C:\temp\file.txt";
+        FileStream fs = null;
+        StreamReader sr = null;
+
+        try
+        {
+            fs = new FileStream(path, FileMode.Open);
+            sr = new StreamReader(fs);
+
+            string line = sr.ReadLine();
+
+            Console.WriteLine(line);
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred");
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            if (sr != null) sr.Close();
+            if (fs != null) fs.Close();
+        }
     }
 }
